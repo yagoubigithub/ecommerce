7d872090d3ced2b2a2e66b45231caec93dab73d5
yagoubigithub <yagoubi.aek.2@gmail.com>
2021-05-01 05:56:22 +0100

    search product

--- routes/product.js
+++ routes/product.js
@@ -3,7 +3,7 @@ const router  = express.Router();
 
 
 
-const { create ,productById,read,remove , update} = require("../controllers/product")
+const { create ,productById,read,remove , update,list, listRelated,listCategories,listBySearch} = require("../controllers/product")
 const {isAuth, isAdmin ,requireSignin} = require("../controllers/auth")
 const {userById} = require("../controllers/user")
 
@@ -13,6 +13,13 @@ router.get("/product/:productId", read)
 router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create)
 router.delete("/product/:productId/:userId", requireSignin, isAuth, isAdmin, remove)
 router.put("/product/:productId/:userId", requireSignin, isAuth, isAdmin, update)
+
+
+router.get("/products", list)
+router.get("/products/related/:productId", listRelated)
+router.get("/products/categories", listCategories)
+// route - make sure its post
+router.post("/products/by/search", listBySearch);
 
 router.param("userId", userById);
 router.param("productId", productById);
