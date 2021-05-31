0064c0854c715e0faecb57ec73f9a5520a076dcb
yagoubigithub <yagoubi.aek.2@gmail.com>
2021-06-01 00:57:42 +0100

    braintree

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const authRoutes = require("./routes/auth")
 const userRoutes = require("./routes/user")
 const categoryRoutes = require("./routes/category")
 const productRoutes = require("./routes/product")
+const braintreeRoutes = require("./routes/braintree")
 
 
 const app = express();
@@ -41,6 +42,7 @@ app.use("/api",authRoutes)
 app.use("/api",userRoutes)
 app.use("/api",categoryRoutes)
 app.use("/api",productRoutes)
+app.use("/api",braintreeRoutes)
 
 
 
